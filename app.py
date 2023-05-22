3b33800d2b3627799b6a4836cc56c35a70625c8a
ualiu <urim.aliu@gmail.com>
2023-05-22 14:00:40 -0400

    updated code

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from dotenv import load_dotenv
 import pickle
 from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
@@ -10,36 +11,36 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
 import os
 
+
 # Sidebar contents
 with st.sidebar:
-    st.title('LLM Chat App')
-    st.markdown('''
-    ## How to use this app
-    1. Enter your OpenAI API key below
-    2. Upload a pdf, docx, or txt file
-    3. Ask a question about your document
-    ''')
+        add_vertical_space(1)
+        st.write('Created by Urim Aliu')
+        st.markdown('''
+        ## How to use this app
+        1. Upload a pdf, docx, or txt file
+        2. Enter your OpenAI API key below
+        3. Using natural language, ask a question about your document
+        
+        ## FAQ
 
-    api_key = st.text_input('Enter your API key', type='password')
 
-    st.markdown('''
-    ## FAQ
+        **1. How does this app work?**
 
-    **1. How does this app work?** 
 
-    When you upload a document, it will be divided into smaller chunks and stored in a special type of database called a vector index that allows for semantic search and retrieval.
+        When you upload a document, it will be divided into smaller chunks and stored in a special type of database.
 
-    When you ask a question, the app uses an AI model called **pdfGPT** to search through the document chunks and find the most relevant ones using the vector index. Then, it uses another AI model, **GPT3**, to generate a final answer.
-    
-    **1. Is my data safe?** 
 
-    Yes, your data is safe. pdfGPT does not store your documents or questions. All uploaded data is deleted after you close the browser tab.
-    ''')
+        When you ask a question using natural lanauge, the app uses an AI model called **pdfGPT** to search through the document chunks and find the most relevant information. Then, it uses another AI model, **GPT3**, to generate a final answer.
+       
+        **1. Is my data safe?**
 
-    add_vertical_space(1)
-    st.write('Made with ❤️ by Urim Aliu')
-    # Ask the user to enter their API key
 
+        Yes, your data is safe. **pdfGPT** does not store your documents or questions. All uploaded data is deleted after you close the browser tab.
+        ''')
+
+load_dotenv()
+ 
 def main():
     st.header("Chat with your PDFs 💬")
 
@@ -47,46 +48,50 @@ def main():
     pdf = st.file_uploader("Upload your PDF", type='pdf')
 
     if pdf is not None:
-        if not api_key:  # if the user has not entered an API key
-            st.error("API key is required to proceed. Get your OpenAI API key here: https://platform.openai.com/account/api-keys.")
-            return
+        # Ask the user for the API key
+        api_key = st.text_input('Enter your API key', type='password')
 
-        pdf_reader = PdfReader(pdf)
+        # Set the API key as an environment variable
+        os.environ["OPENAI_API_KEY"] = api_key
+        
+        if api_key:
+            pdf_reader = PdfReader(pdf)
+            
+            text = ""
+            for page in pdf_reader.pages:
+                text += page.extract_text()
 
-        text = ""
-        for page in pdf_reader.pages:
-            text += page.extract_text()
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size=1000,
+                chunk_overlap=200,
+                length_function=len
+                )
+            chunks = text_splitter.split_text(text=text)
 
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len
-            )
-        chunks = text_splitter.split_text(text=text)
+            store_name = pdf.name[:-4]
+    
+            if os.path.exists(f"{store_name}.pkl"):
+                with open(f"{store_name}.pkl", "rb") as f:
+                    VectorStore = pickle.load(f)
+            else:
+                embeddings = OpenAIEmbeddings()
+                VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+                with open(f"{store_name}.pkl", "wb") as f:
+                    pickle.dump(VectorStore, f)
 
-        store_name = pdf.name[:-4]
-        st.write(f'{store_name}')
+            query = st.text_input("Ask questions about your PDF file:")
+    
+            if query:
+                docs = VectorStore.similarity_search(query=query, k=3)
 
-        if os.path.exists(f"{store_name}.pkl"):
-            with open(f"{store_name}.pkl", "rb") as f:
-                VectorStore = pickle.load(f)
+                llm = OpenAI()
+                chain = load_qa_chain(llm=llm, chain_type="stuff")
+                with get_openai_callback() as cb:
+                    response = chain.run(input_documents=docs, question=query)
+                    print(cb)
+                st.write(response)
         else:
-            embeddings = OpenAIEmbeddings(api_key=api_key)  # use the provided API key
-            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-            with open(f"{store_name}.pkl", "wb") as f:
-                pickle.dump(VectorStore, f)
-
-        query = st.text_input("Ask questions about your PDF file:")
-
-        if query:
-            docs = VectorStore.similarity_search(query=query, k=3)
-
-            llm = OpenAI(api_key=api_key)  # use the provided API key
-            chain = load_qa_chain(llm=llm, chain_type="stuff")
-            with get_openai_callback() as cb:
-                response = chain.run(input_documents=docs, question=query)
-                print(cb)
-            st.write(response)
-
+            st.write("Please enter your API key to use the app.")
+ 
 if __name__ == '__main__':
     main()
